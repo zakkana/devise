0891d2fc6fbb4d92562017273a17d257c2309106
zakkana <IT@LAPTOP-G91R601H.localdomain>
2018-02-27 16:07:40 +0900

    メールが実際に届くようにする

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,8 +59,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address => "smtp.gmail.com",
     :port => 587,
-    :user_name => "playing.the.lone.wolf.as.alwa@gmail.com"
-    :password => "hjeeexhoytavbwrf"
+    :user_name => "playing.the.lone.wolf.as.alwa@gmail.com",
+    :password => "hjeeexhoytavbwrf",
     :authentication => :plain,
     :enable_starttls_auto => true
   }
