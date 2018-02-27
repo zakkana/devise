c0a3e9ba5e58fd282fd0a408a478be1fd9b350db
zakkana <IT@LAPTOP-G91R601H.localdomain>
2018-02-27 14:48:42 +0900

    ログイン後のページを変更する

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  def after_sign_in_path_for(resourse)
+    pages_show_path
+  end
+
+  private
+    def sign_in_required
+      redirect_to new_user_session_url unless user_signed_in?
+    end
 end
