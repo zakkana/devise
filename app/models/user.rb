68e4ebe17e297cbaacbe60ef3901a595f85901ea
zakkana <IT@LAPTOP-G91R601H.localdomain>
2018-04-03 22:22:32 +0900

    Twitterでログインしたときのcallbackの呼び出しを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,21 +5,22 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable, :lockable, :timeoutable, :omniauthable, omniauth_providers: [:twitter]
 
-  def self.from_omniauth(auth)
-    find_or_create_by(provider: auth["provider"], uid: auth["uid"]) do |user|
-      user.provider = auth["provider"]
-      user.uid = auth["uid"]
-      user.username =auth["info"]["nickname"]
-    end
-  end
+      def self.from_omniauth(auth)
+          where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+               user.provider = auth["provider"]
+               user.uid = auth["uid"]
+               user.username = auth["info"]["nickname"]
+           end
+       end
 
-  def self.new_with_session(params, session)
-    if session["devise.user_attributes"]
-      new(session["devise.user_attributes"]) do |user|
-        user.attributes = params
-      end
-    else
-      super
-    end
-  end
-end
+       def self.new_with_session(params, session)
+           if session["devise.user_attributes"]
+               new(session["devise.user_attributes"]) do |user|
+                   user.attributes = params
+                   user.valid?
+               end
+           else
+               super
+           end
+       end
+     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#index'
   get 'pages/show'
-  devise_for :users, controller: { :omniauth_callbacks => "omniauth_callbacks"}
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks"}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
